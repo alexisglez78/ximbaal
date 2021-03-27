4f0938da1caedfa726c7eaca5293eed6b5639f37
alexisglez <alexisglez78@gmail.com>
2021-03-27 14:04:43 -0600

    se agrego el modal de la galeria pero no esta terminado

--- screens/DetalleSite.tsx
+++ screens/DetalleSite.tsx
@@ -19,7 +19,7 @@ export default function DetalleSite({
   const [lugar, useLugar] = React.useState();
   const [nombre, setNombre] = React.useState();
   const [detalle, setDetalle] = React.useState();
-  const [imagen,setImagen] = React.useState({});
+  const [imagen, setImagen] = React.useState({});
 
   React.useEffect(() => {
     fetch('https://ludotecapiccolo.000webhostapp.com/detalle.php', {
@@ -33,144 +33,150 @@ export default function DetalleSite({
         useLugar(data[0]);
         setNombre(data[0].nombre);
         setDetalle(data[0].detalle);
-        setImagen({uri:data[0].imagen});
+        setImagen({ uri: data[0].imagen });
       })
   }, []);
-  const _renderItem = ({item, index}) => {
+  const _renderItem = ({ item, index }) => {
     return (
-        <View >
-            <Text style={styles.title}>{ item.title }</Text>
-        </View>
-    );
-}
-  return (
-    <View style={{height:"100%"}}>
-    <ScrollView style={{height:"90%"}}>
-
-      <View style={{width:'100%',backgroundColor:'white'}}>
-        <ScrollView horizontal={true} >
-          <View style={{width:360,height:200}}>
-        <ImageBackground source={imagen} style={{height: 200,width:'100%'}}>
-          <HeaderDetalle
-          onPress={() => navigation.replace('Root')} 
-          />
-          </ImageBackground>
-          </View>
-        </ScrollView>
-        <View style={styles.center}>
-          <Text style={styles.name}>{nombre}</Text>
-        </View>
-        <View style={{flex:1}}>
-        <Text style={styles.detalle}>{detalle}</Text>
-        </View>
-        <View style={styles.viewPrincipalIcons}>
-          <View style={styles.vistaIcons}><Text style={styles.alignIcons}><Avatar.Icon icon="star-outline" size={45} color="#696969" style={{backgroundColor:'transparent'}}/></Text><Text style={styles.textIcons}>197 veces agregado a favoritos</Text></View>
-          <View style={styles.vistaIcons}><Text style={styles.alignIcons}><Avatar.Icon icon="account-group-outline" size={45} color="#696969" style={{backgroundColor:'transparent'}}/></Text><Text style={styles.textIcons}>Hasta 6 personas</Text></View>
-          <View style={styles.vistaIcons}><Text style={styles.alignIcons}><Avatar.Icon icon="eye" size={45} color="#696969" style={{backgroundColor:'transparent'}}/></Text><Text style={styles.textIcons}>325 Visualizaciones</Text></View>
-          <View style={styles.vistaIcons}><Text style={styles.alignIcons}><Avatar.Icon icon="heart-outline" size={45} color="#696969" style={{backgroundColor:'transparent'}}/></Text><Text style={styles.textIcons}>A 186 les gusta</Text></View>
-        </View>
-        <View style={{}}>
-        <PreciosComponent></PreciosComponent>
-        </View>
-
-        <ScrollView horizontal={true} style={{}}>
-        <View style={{marginLeft: 8,
-    height: 130,
-    width: 330,
-    borderRadius: 20,}}>
-        <Cards title={
-       <View style={{flex:1,flexDirection:'row',marginTop:50}}>
-         <View style={styles.stars} >
-         <Avatar.Icon icon="star" size={30} color="#f6c800" style={{backgroundColor:'transparent'}}/>
-         </View>
-         <View style={styles.stars} >
-         <Avatar.Icon icon="star" size={30} color="#f6c800" style={{backgroundColor:'transparent'}}/>
-         </View>
-         <View style={styles.stars} >
-         <Avatar.Icon icon="star" size={30} color="#f6c800" style={{backgroundColor:'transparent'}}/>
-         </View>
-         <View style={styles.stars} >
-         <Avatar.Icon icon="star" size={30} color="#696969" style={{backgroundColor:'transparent'}}/>
-         </View>
-         <View style={styles.stars} >
-         <Avatar.Icon icon="star" size={30} color="#696969" style={{backgroundColor:'transparent'}}/>
-         </View>
-       </View>
-        } 
-        subtitle="El lugar esta muy padre pero no venden cahuamas cerquita  :3"></Cards>
-        </View>
-        <View style={{marginLeft: 8,
-    height: 130,
-    width: 330,
-    borderRadius: 20,}}>
-        <Cards title={
-       <View style={{flex:1,flexDirection:'row',marginTop:50}}>
-         <View style={styles.stars} >
-         <Avatar.Icon icon="star" size={30} color="#f6c800" style={{backgroundColor:'transparent'}}/>
-         </View>
-         <View style={styles.stars} >
-         <Avatar.Icon icon="star" size={30} color="#f6c800" style={{backgroundColor:'transparent'}}/>
-         </View>
-         <View style={styles.stars} >
-         <Avatar.Icon icon="star" size={30} color="#f6c800" style={{backgroundColor:'transparent'}}/>
-         </View>
-         <View style={styles.stars} >
-         <Avatar.Icon icon="star" size={30} color="#696969" style={{backgroundColor:'transparent'}}/>
-         </View>
-         <View style={styles.stars} >
-         <Avatar.Icon icon="star" size={30} color="#696969" style={{backgroundColor:'transparent'}}/>
-         </View>
-       </View>
-        } 
-        subtitle="El lugar esta muy padre pero no venden cahuamas cerquita  :3"></Cards>
-        </View>
-      
-        </ScrollView>
-        <View style={styles.viewSecundario}>
-        {/* <Formulario></Formulario> */}
-
-        </View>
+      <View >
+        <Text style={styles.title}>{item.title}</Text>
       </View>
-    </ScrollView>
-    <View style={{width:'100%',flex:15,flexDirection:'row',backgroundColor:'white',borderWidth:1,
-borderTopColor:'#d8d8d8',
-borderRightColor:'transparent',
-borderLeftColor:'transparent',
-borderBottomColor:'transparent',
-paddingTop:'2%'}}>
-    <View style={{width:'50%',alignItems:'center',alignContent:'center'}}>
-        <View style={{
-          //  elevation: 1,
-           backgroundColor: "white",
-           borderRadius: 5,
-           paddingVertical: 5,
-           paddingHorizontal: 20
-        }}>
-        <Text style={{fontWeight:'bold',color:'red'}}>$1800.00 mxn</Text>
-        <Text > por persona</Text>
+    );
+  }
+  return (
+    <View style={{ height: "100%" }}>
+      <ScrollView style={{ height: "90%" }}>
+
+        <View style={{ width: '100%', backgroundColor: 'white' }}>
+          <ScrollView horizontal={true} >
+            <View style={{ width: 360, height: 200 }}>
+              <ImageBackground source={imagen} style={{ height: 200, width: '100%' }}>
+                <HeaderDetalle
+                  onPress={() => navigation.replace('Root')}
+                />
+              </ImageBackground>
+            </View>
+          </ScrollView>
+          <View style={styles.center}>
+            <Text style={styles.name}>{nombre}</Text>
+          </View>
+          <View style={{ flex: 1 }}>
+            <Text style={styles.detalle}>{detalle}</Text>
+          </View>
+          <View style={styles.viewPrincipalIcons}>
+            <View style={styles.vistaIcons}><Text style={styles.alignIcons}><Avatar.Icon icon="star-outline" size={45} color="#696969" style={{ backgroundColor: 'transparent' }} /></Text><Text style={styles.textIcons}>197 veces agregado a favoritos</Text></View>
+            <View style={styles.vistaIcons}><Text style={styles.alignIcons}><Avatar.Icon icon="account-group-outline" size={45} color="#696969" style={{ backgroundColor: 'transparent' }} /></Text><Text style={styles.textIcons}>Hasta 6 personas</Text></View>
+            <View style={styles.vistaIcons}><Text style={styles.alignIcons}><Avatar.Icon icon="eye" size={45} color="#696969" style={{ backgroundColor: 'transparent' }} /></Text><Text style={styles.textIcons}>325 Visualizaciones</Text></View>
+            <View style={styles.vistaIcons}><Text style={styles.alignIcons}><Avatar.Icon icon="heart-outline" size={45} color="#696969" style={{ backgroundColor: 'transparent' }} /></Text><Text style={styles.textIcons}>A 186 les gusta</Text></View>
+          </View>
+          <View style={{}}>
+            <PreciosComponent></PreciosComponent>
+          </View>
+
+          <ScrollView horizontal={true} style={{}}>
+            <View style={{
+              marginLeft: 8,
+              height: 130,
+              width: 330,
+              borderRadius: 20,
+            }}>
+              <Cards title={
+                <View style={{ flex: 1, flexDirection: 'row', marginTop: 50 }}>
+                  <View style={styles.stars} >
+                    <Avatar.Icon icon="star" size={30} color="#f6c800" style={{ backgroundColor: 'transparent' }} />
+                  </View>
+                  <View style={styles.stars} >
+                    <Avatar.Icon icon="star" size={30} color="#f6c800" style={{ backgroundColor: 'transparent' }} />
+                  </View>
+                  <View style={styles.stars} >
+                    <Avatar.Icon icon="star" size={30} color="#f6c800" style={{ backgroundColor: 'transparent' }} />
+                  </View>
+                  <View style={styles.stars} >
+                    <Avatar.Icon icon="star" size={30} color="#696969" style={{ backgroundColor: 'transparent' }} />
+                  </View>
+                  <View style={styles.stars} >
+                    <Avatar.Icon icon="star" size={30} color="#696969" style={{ backgroundColor: 'transparent' }} />
+                  </View>
+                </View>
+              }
+                subtitle="El lugar esta muy padre pero no venden cahuamas cerquita  :3"></Cards>
+            </View>
+            <View style={{
+              marginLeft: 8,
+              height: 130,
+              width: 330,
+              borderRadius: 20,
+            }}>
+              <Cards title={
+                <View style={{ flex: 1, flexDirection: 'row', marginTop: 50 }}>
+                  <View style={styles.stars} >
+                    <Avatar.Icon icon="star" size={30} color="#f6c800" style={{ backgroundColor: 'transparent' }} />
+                  </View>
+                  <View style={styles.stars} >
+                    <Avatar.Icon icon="star" size={30} color="#f6c800" style={{ backgroundColor: 'transparent' }} />
+                  </View>
+                  <View style={styles.stars} >
+                    <Avatar.Icon icon="star" size={30} color="#f6c800" style={{ backgroundColor: 'transparent' }} />
+                  </View>
+                  <View style={styles.stars} >
+                    <Avatar.Icon icon="star" size={30} color="#696969" style={{ backgroundColor: 'transparent' }} />
+                  </View>
+                  <View style={styles.stars} >
+                    <Avatar.Icon icon="star" size={30} color="#696969" style={{ backgroundColor: 'transparent' }} />
+                  </View>
+                </View>
+              }
+                subtitle="El lugar esta muy padre pero no venden cahuamas cerquita  :3"></Cards>
+            </View>
+
+          </ScrollView>
+          <View style={styles.viewSecundario}>
+            {/* <Formulario></Formulario> */}
+
+          </View>
         </View>
+      </ScrollView>
+      <View style={{
+        width: '100%', flex: 15, flexDirection: 'row', backgroundColor: 'white', borderWidth: 1,
+        borderTopColor: '#d8d8d8',
+        borderRightColor: 'transparent',
+        borderLeftColor: 'transparent',
+        borderBottomColor: 'transparent',
+        paddingTop: '2%'
+      }}>
+        <View style={{ width: '50%', alignItems: 'center', alignContent: 'center' }}>
+          <View style={{
+            //  elevation: 1,
+            backgroundColor: "white",
+            borderRadius: 5,
+            paddingVertical: 5,
+            paddingHorizontal: 20
+          }}>
+            <Text style={{ fontWeight: 'bold', color: 'red' }}>$1800.00 mxn</Text>
+            <Text > por persona</Text>
+          </View>
         </View>
-        <View style={{width:'50%',alignItems:'center',alignContent:'center'}}>
-        <TouchableOpacity style={{
-           elevation: 8,
-           backgroundColor: "#009688",
-           borderRadius: 5,
-           paddingVertical: 11,
-           paddingHorizontal: 15
-        }}
-        onPress={() => 
-          navigation.navigate('DetalleReservacion', {
-            itemId:1997,
-            otherParam: 'parametro',
-          })
-        } 
-        >
-          <Text style={{
-            fontSize: 20,
-            color: "#fff",
-            // fontWeight: "bold",
-            alignSelf: "center",
-          }}>Reservar</Text>
+        <View style={{ width: '50%', alignItems: 'center', alignContent: 'center' }}>
+          <TouchableOpacity style={{
+            elevation: 8,
+            backgroundColor: "#009688",
+            borderRadius: 5,
+            paddingVertical: 11,
+            paddingHorizontal: 15
+          }}
+            onPress={() =>
+              navigation.navigate('DetalleReservacion', {
+                itemId: 1997,
+                otherParam: 'parametro',
+              })
+            }
+          >
+            <Text style={{
+              fontSize: 20,
+              color: "#fff",
+              // fontWeight: "bold",
+              alignSelf: "center",
+            }}>Reservar</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -182,60 +188,60 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
   },
-  center:{
+  center: {
     alignItems: 'center'
   },
-  alignIcons:{
+  alignIcons: {
     textAlign: 'center'
   },
-  textIcons:{
-    marginLeft:13,
-    color:'#696969',
-    fontSize:10,
-    marginTop:-8
+  textIcons: {
+    marginLeft: 13,
+    color: '#696969',
+    fontSize: 10,
+    marginTop: -8
   },
-  name:{
-    fontSize:28,
-    color:"#353535",
-    fontWeight:'bold',
-    
+  name: {
+    fontSize: 28,
+    color: "#353535",
+    fontWeight: 'bold',
+
   },
- 
+
   imagen: {
     height: 200,
     borderBottomRightRadius: 20,
     borderBottomLeftRadius: 20,
     resizeMode: 'center'
   },
-  detalle:{
-    fontSize:19,
-    color:"#353535",
-    marginLeft:24,
-    marginRight:20,
-    
+  detalle: {
+    fontSize: 19,
+    color: "#353535",
+    marginLeft: 24,
+    marginRight: 20,
+
   },
-  vistaIcons:{
-    width:70,
-    height:90,
-    margin:5,
-    textAlign:'center'
+  vistaIcons: {
+    width: 70,
+    height: 90,
+    margin: 5,
+    textAlign: 'center'
   },
-  viewPrincipalIcons:{
-   flexDirection:"row",
-   flex:1,
-   alignContent: "center",
-   marginLeft:10,
-   marginRight:10,
-   textAlign:'center',
-   paddingLeft:5
+  viewPrincipalIcons: {
+    flexDirection: "row",
+    flex: 1,
+    alignContent: "center",
+    marginLeft: 10,
+    marginRight: 10,
+    textAlign: 'center',
+    paddingLeft: 5
   },
-  viewSecundario:{
-    marginLeft:10,
-    marginRight:10,
-    
+  viewSecundario: {
+    marginLeft: 10,
+    marginRight: 10,
+
   },
-  stars:{
-    width: 20, 
+  stars: {
+    width: 20,
     height: 50,
   },
 });
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,132 +1,286 @@
 import * as React from 'react';
-import { Platform, Image, ImageBackground, StyleSheet, TouchableOpacity } from 'react-native';
+import {
+  Platform,
+  Image,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+  Pressable,
+} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import Mapa from '../components/Mapa';
+import { Text, View, Modal } from 'react-native';
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import MasonryList from "react-native-masonry-list";
 
-const image={uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPNMW94Mz2ja54dMop9fj4ypaOeeIFpDeS7Q&usqp=CAU'}
-const profile ={uri:'https://imagenes.20minutos.es/files/image_656_370/uploads/imagenes/2020/01/27/influencer-con-pingueinos-apareandose.jpeg'}
 
-const Caja = (props:any) => {
-  return (
-    <TouchableOpacity onPress={() => {alert(props.info)}}>
-    <View style={styles.infContainer}>
-    <View>
-  <Text style={styles.titulo}>{props.titulo}</Text>
-  <Text style={styles.subtitulo}>{props.info}</Text>
-   <Text></Text>
-    </View>
-  </View>
-    </TouchableOpacity>
-  );
-}
+
+
+
+const image = {
+  uri:
+    'https://i.hipertextual.com/2017/03/22/4ff902d76bb370a7b40756aa6dd65c11.md.jpg',
+};
+const profile = {
+  uri:
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS316qTwul1gW8pZ45UJ2WbhL5MuujJbfIuKg&usqp=CAU',
+};
+
 
 export default function TabTwoScreen() {
+  const [modalVisible, setModalVisible] = React.useState(false);
+
+  const AbrirImagen = (param) => {
+    console.log(param)
+    setModalVisible(true)
+  }
   return (
-   <ScrollView>
-     <View>
-       <View style={{alignContent:'center'}}>
-          <ImageBackground source={image} style={styles.imagen}>
+    <View style={{ height: '100%', alignItems: 'center' }} >
+      <ScrollView style={{ height: '90%' }}>
+        <View>
+          <View >
+            <ImageBackground source={image} style={styles.imagen}>
+              <View style={{ width: '100%' }}>
+                <Text style={styles.profileText}>Perfil</Text>
+              </View>
+            </ImageBackground>
+          </View>
+        </View>
+        <View style={{ backgroundColor: 'transparent', marginTop: '-18%' }}>
+          <Card style={{ margin: 10, height: 200 }}>
             <View style={styles.divProfile}>
-            <Image source={profile} style={styles.profile}></Image>
+              <Image source={profile} style={styles.profile}></Image>
             </View>
-          <Text style={styles.name}>Nombre de persona  <TouchableOpacity style={styles.buttonIcon}>
-            <FontAwesome  name="pencil" size={25} color="#fff" ></FontAwesome>
-            </TouchableOpacity>
-            </Text>
-          </ImageBackground>
-       </View>
-       <Caja
-    titulo="Nombre"
-    info="Alexis"
-    />
-       <Caja
-    titulo="Apellido"
-    info="Glez"
-    />
-    <Caja
-    titulo="Se unio en"
-    info="23 de febrero del 2020"
-    />
-    <Caja
-    titulo="Idiomas"
-    info="Español"
-    />
-    <Caja
-    titulo="Correo electronico"
-    info="prueba@gmail.com"
-    />
-    <Caja
-    titulo="Telefono movil"
-    info="5574668888"
-    />
-     </View>
-     <View>
-    <Mapa></Mapa>
-     </View>
-   </ScrollView>
+            <View style={{ width: '100%', height: 60 }}>
+              <TouchableOpacity onPress={() => { alert('Search!') }}>
+                <Text
+                  style={{
+                    margin: 5,
+
+                    height: '40%',
+                  }}>
+                  <Avatar.Icon
+                    icon="settings"
+                    size={45}
+                    color="#696969"
+                    style={{ backgroundColor: 'transparent' }}
+                  />
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                width: '100%',
+                alignItems: 'center',
+                borderBottomWidth: 1, borderColor: '#e1e0e0',
+                paddingBottom: 10
+              }}>
+              <Text style={{ fontSize: 25, color: '#696969' }}>
+                Paul Alexis
+              </Text>
+              <Text style={{ color: '#a3a3a3' }}>
+                alexisglez78@gmail.com
+              </Text>
+            </View>
+            <View style={{}}>
+              <TouchableOpacity style={{ flex: 1, flexDirection: 'row', margin: 5 }}>
+                <View
+                  style={{
+                    width: '12%',
+                    height: 50,
+                  }}>
+                  <Avatar.Icon
+                    icon="phone"
+                    size={45}
+                    color="#a3a3a3"
+                    style={{ backgroundColor: 'transparent' }}
+                  />
+
+                </View>
+                <View
+                  style={{
+                    width: '78%',
+                    height: 50,
+                  }}
+                >
+                  <Text style={{ fontSize: 20, color: '#a3a3a3', marginTop: 8 }}>
+                    5574518878
+                    </Text>
+                </View>
+                <View
+                  style={{
+                    width: '10%',
+                    height: 50,
+                  }}
+                >
+                  <Avatar.Icon
+                    icon="chevron-right"
+                    size={45}
+                    color="#a3a3a3"
+                    style={{ backgroundColor: 'transparent' }}
+                  />
+                </View>
+              </TouchableOpacity>
+            </View>
+          </Card>
+        </View>
+        <View>
+          <Card style={{ marginLeft: 10, marginRight: 10 }}>
+            {/* <Text style={{ marginLeft: 20, fontSize: 20, color: '#696969' }}>Galeria</Text> */}
+            <MasonryList
+              onPressImage={({ source, index }) => AbrirImagen(source.uri)}
+              images={[
+                // Can be used with different image object fieldnames.
+                // Ex. source, source.uri, uri, URI, url, URL
+                { uri: "https://d500.epimg.net/cincodias/imagenes/2020/09/04/lifestyle/1599210924_839777_1599210984_rrss_normal.jpg" },
+
+                {
+                  source: { uri: "https://luehangs.site/pic-chat-app-images/beautiful-beautiful-women-beauty-40901.jpg" }, dimensions: { width: 1080, height: 1920 }
+                },
+                {
+                  source: { uri: "https://luehangs.site/pic-chat-app-images/beautiful-beautiful-women-beauty-40901.jpg" }, dimensions: { width: 90, height: 90 }
+                },
+                {
+                  uri: "https://luehangs.site/pic-chat-app-images/animals-avian-beach-760984.jpg",
+                },
+                {
+                  URI: "https://luehangs.site/pic-chat-app-images/beautiful-blond-fishnet-stockings-48134.jpg",
+                  // Optional: Does not require an id for each
+                  // image object, but is for best practices.
+                  id: "blpccx4cn"
+                },
+                { URL: "https://luehangs.site/pic-chat-app-images/attractive-balance-beautiful-186263.jpg" },
+                { url: "https://luehangs.site/pic-chat-app-images/beautiful-beautiful-woman-beauty-9763.jpg" },
+              ]}
+            />
+          </Card>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={{ width: '100%', height: '100%', backgroundColor: '#000000bd' }}>
+              <View style={{ flex: 1, flexDirection: 'row', opacity: 0.7 }}>
+
+                <View style={{ width: '80%', height: 50 }} >
+                  <TouchableOpacity
+                    style={styles.link}
+                    onPress={() => setModalVisible(!modalVisible)}>
+                    <Text style={{ marginLeft: -5.0, marginTop: -5.0 }}>
+                      <Avatar.Icon icon="arrow-left" size={45} color="#000000" style={{ backgroundColor: 'transparent' }} />
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <View style={{ width: '10%', height: 50 }} >
+                  <TouchableOpacity style={styles.link}
+                  >
+                    <Text style={{ marginLeft: -4.9, marginTop: -6.0 }}>
+                      {/* <Ionicons name="share-vlariant" size={45} color="#000000"  /> */}
+                      <Avatar.Icon icon="export-variant" size={45} color="#000000" style={{ backgroundColor: 'transparent' }} />
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+
+              </View>
+
+              <Text >Hello World!</Text>
+              <Pressable
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text >Hide Modal</Text>
+                <Image source={profile} style={styles.profile}></Image>
+              </Pressable>
+            </View>
+          </Modal>
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   imagen: {
     height: 200,
+    width: '100%',
     borderBottomRightRadius: 20,
-    borderBottomLeftRadius: 20,
+    borderBottomLeftRadius: 50,
     resizeMode: 'center',
-    alignItems:'center'
+    alignItems: 'center',
   },
-  profile:{
-    width:120,
-    height:120,
-    backgroundColor:'transparent',
-    borderRadius:60,
+  profile: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'transparent',
+    borderRadius: 60,
+    borderColor: 'blue',
   },
-  divProfile:{
-    backgroundColor:'transparent',
-    width:10,
-    height:10,
-    marginTop:25,
-    alignItems:'center',
+  divProfile: {
+    backgroundColor: 'transparent',
+    width: '100%',
+    height: 50,
+    marginTop: '-16%',
+    alignItems: 'center',
   },
-  name:{
-    fontSize:28,
-    color:"#696969",
-    fontWeight:'bold',
-    marginTop:115,
+  name: {
+    fontSize: 28,
+    color: '#696969',
+    fontWeight: 'bold',
+    marginTop: 115,
   },
-  buttonIcon:{
-    borderRadius:4,
-    backgroundColor:'#0080ff',
-    alignItems:'center',
-    textAlign:'center',
-    width:35,
-    height:30
+  link: {
+    width: 35,
+    height: 35,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    marginTop: 30,
+    marginLeft: 20,
+
   },
-  infContainer:{
-padding:5,
-marginLeft:10,
-marginRight:10,
-borderBottomWidth:2,
-borderColor:'#696969',
-marginBottom:5
+  iconos: {
+    height: 40,
+    marginTop: -5,
+    marginLeft: 4
   },
-  titulo:{
+  buttonIcon: {
+    borderRadius: 4,
+    backgroundColor: '#0080ff',
+    alignItems: 'center',
+    textAlign: 'center',
+    width: 35,
+    height: 30,
+  },
+  infContainer: {
+    padding: 5,
+    marginLeft: 10,
+    marginRight: 10,
+    borderBottomWidth: 2,
+    borderColor: '#696969',
+    marginBottom: 5,
+  },
+  titulo: {
     fontSize: 20,
-      color:'#353a42',
-    fontWeight:'bold',
+    color: '#353a42',
+    fontWeight: 'bold',
   },
-  subtitulo:{
-    fontSize:17,
-    color:"#333333",
-},
+  subtitulo: {
+    fontSize: 17,
+    color: '#333333',
+  },
+  profileText: {
+    color: 'white',
+    marginTop: '15%',
+    fontSize: 30,
+    marginLeft: 10,
+    fontWeight: 'bold',
+  },
 });
 
-// https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPNMW94Mz2ja54dMop9fj4ypaOeeIFpDeS7Q&usqp=CAU 
+// https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPNMW94Mz2ja54dMop9fj4ypaOeeIFpDeS7Q&usqp=CAU
 
-// se unio 
+// se unio
 // ubicacion
 // idioma
